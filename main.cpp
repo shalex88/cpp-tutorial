5c7b630cb86d839dde6091a4ffd6d9b4d4be4416
Alex Shevtsov <shalex.work@gmail.com>
2021-10-09 23:33:00 +0300

    First challenge

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int favorite_number = 0;
+
+    std::cout << "Enter your favorite number from 1 to 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!! " << favorite_number << " is my favorite number!" << std::endl;
+
     return 0;
 }
