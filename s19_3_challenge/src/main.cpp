bb33577ea303aec737e53f0ae19185e17407aa02
Alex Sh <shalex.work@gmail.com>
2022-02-05 11:57:55 +0200

    Add s19_3_challenge

---
+++ s19_3_challenge/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+int main() {
+  std::string filename {"..//..//s19_3_challenge//files//romeoandjuliet.txt"};
+  std::ifstream in_file {filename};
+  if (!in_file) {
+	std::cerr << "Error opening file" << std::endl;
+	return 1;
+  }
+
+  /* Get the word to word_to_find */
+  std::string word_to_find {};
+  std::cout << "Enter a word to find: ";
+  std::cin >> word_to_find;
+
+  std::string current_word {};
+  int match_count {};
+  int total_count {};
+
+  while (in_file >> current_word) {
+	total_count++;
+	size_t found_position = current_word.find(word_to_find);
+	if (found_position != std::string::npos) {
+	  match_count++;
+	}
+  }
+
+  in_file.close();
+
+  std::cout << "Match counter: " << match_count << " / " << total_count << std::endl;
+
+  return 0;
+}
